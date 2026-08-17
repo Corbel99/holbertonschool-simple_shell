a623c128965f3a8ca1aaeba4142900a7abb8dc99
Corbel99 <thomas.corbel35000@gmail.com>
2026-08-17 10:14:37 +0200

    git commit -m "Add basic shell loop with getline and execve"

--- simple_shell.c
+++ simple_shell.c
@@ -1,9 +1,72 @@
-#include "simple_shell.h"
-int main(int ac, char **av, char **envp)
-{
-	(void)ac;
-	(void)av;
-    (void)envp;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+/**
+ * main - simple shell that executes commands with their full path
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv, char **envp)
+{
+	char *line = NULL;
+	size_t len = 0;
+	pid_t pid;
+	int status;
+	char **exec_argv;
+
+	(void)argc;
+	(void)argv;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+
+		if (getline(&line, &len, stdin) == -1)
+			break;
+
+		len = strlen(line);
+
+		if (line[len - 1] == '\n')
+        {
+            line[len - 1] = '\0';
+        }
+
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("fork");
+			return (1);
+		}
+
+		if (pid == 0)
+		{
+			exec_argv = malloc(sizeof(char *) * 2);
+
+			if (exec_argv == NULL)
+			{
+				perror("malloc");
+				return (1);
+			}
+
+			exec_argv[0] = line;
+			exec_argv[1] = NULL;
+
+			execve(line, exec_argv, envp);
+			perror("execve");
+			free(exec_argv);
+			return (1);
+		}
+
+		if (pid > 0)
+		{
+			wait(&status);
+		}
+	}
+
+	free(line);
 	return (0);
 }
--- simple_shell.h
+++ simple_shell.h
@@ -1,4 +1,10 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
+char *read_line(void);
+char **parse_line(char *line);
+int check_builtin(char **argv);
+char *find_path(char *command, char **envp);
+int execute_command(char *pathname, char **argv, char **envp);
+
 #endif
